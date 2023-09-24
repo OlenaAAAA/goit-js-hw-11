ccb593c4d64f553569f8f1c13dc2c10d44aab06f
OlenaAAAA <130498592+OlenaAAAA@users.noreply.github.com>
2023-09-24 14:39:59 +0300

    fixed

--- src/js/index.js
+++ src/js/index.js
@@ -22,59 +22,71 @@ let page = 1;
 let query = '';
 
 
+// функція пошуку зображень при сабміті
+//  ***********   синтаксис із async ... await  and    try ... catch   ***********
+
 
 async function onSearch(event) {
-     event.preventDefault();
+  event.preventDefault();
 
-     refs.gallery.innerHTML = '';
-     page = 1;
-     query = event.currentTarget.elements.searchQuery.value;
-           // console.log(query);
+  refs.gallery.innerHTML = '';
+  page = 1;
+  query = event.currentTarget.elements.searchQuery.value.trim();
+  // console.log(query);
   
-      if (query === '') {
-      Notiflix.Notify.info('Enter your request, please!');
-      return;
-      };
+  if (query === '') {
+    Notiflix.Notify.info('Enter your request, please!');
+    return;
+  };
   
-  await fetchPhoto(query, page, perPage).then(data => {
-    if (data.totalHits === 0) {
+  try {
+    const searchResult = await fetchPhoto(query, page, perPage);
+    // console.log(searchResult);
+    // console.log(searchResult.hits);
+
+    if (searchResult.totalHits === 0) {
       Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      // додано
+      refs.btnLoadMore.classList.add('is-hidden');
+
     }
          
     else {
-      Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
-      refs.gallery.insertAdjacentHTML('beforeend', renderGalleryCards(data.hits));
+      Notiflix.Notify.info(`Hooray! We found ${searchResult.totalHits} images.`);
+      refs.gallery.insertAdjacentHTML('beforeend', renderGalleryCards(searchResult.hits));
       lightbox.refresh();
     };
       
-    if (data.totalHits > perPage) {
+    if (searchResult.totalHits > perPage) {
       refs.btnLoadMore.classList.remove('is-hidden');
     };
     
-  }).catch(onFetchError);
+  }
+  catch (error) {
+    onFetchError();
+  }
   
   refs.btnLoadMore.addEventListener('click', onLoadMore);
   
 };
        
 
-
-
+// функція пошуку при кліку на Load more
 
 async function onLoadMore() {
   page += 1;
-  await fetchPhoto(query, page, perPage).then(data => {
-    const numberOfPage = Math.ceil(data.totalHits / perPage);
-    refs.gallery.insertAdjacentHTML('beforeend', renderGalleryCards(data.hits));
+  try {
+    const searchResult = await fetchPhoto(query, page, perPage);
+    const numberOfPage = Math.ceil(searchResult.totalHits / perPage);
+    
+    refs.gallery.insertAdjacentHTML('beforeend', renderGalleryCards(searchResult.hits));
     lightbox.refresh();
 
     if (page === numberOfPage) {
       refs.btnLoadMore.classList.add('is-hidden');
       Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-      // btnLoadMore.removeEventListener('click', onClickLoadMore);
-    };
-  })
-    .catch(onFetchError);
+      };
+  } catch { onFetchError() };
 };
 
 
@@ -84,3 +96,83 @@ async function onLoadMore() {
 function onFetchError() {
     Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page or make another choice!');
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  ***********   синтаксис із із використанням    .then().catch() ***********
+
+// function onSearch(event) {
+//      event.preventDefault();
+
+//      refs.gallery.innerHTML = '';
+//      page = 1;
+//      query = event.currentTarget.elements.searchQuery.value.trim();
+//            // console.log(query);
+  
+//       if (query === '') {
+//       Notiflix.Notify.info('Enter your request, please!');
+//       return;
+//       };
+  
+//   fetchPhoto(query, page, perPage).then(data => {
+//     if (data.totalHits === 0) {
+//       Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+//       // додано
+//       refs.btnLoadMore.classList.add('is-hidden');
+
+//     }
+         
+//     else {
+//       Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
+//       refs.gallery.insertAdjacentHTML('beforeend', renderGalleryCards(data.hits));
+//       lightbox.refresh();
+//     };
+      
+//     if (data.totalHits > perPage) {
+//       refs.btnLoadMore.classList.remove('is-hidden');
+//     };
+    
+//   }).catch(onFetchError);
+  
+//   refs.btnLoadMore.addEventListener('click', onLoadMore);
+  
+// };
+
+
+
+
+
+
+//  ***********   синтаксис function onLoadMore() із використанням .then().catch() ***********
+
+// function onLoadMore() {
+//   page += 1;
+//   fetchPhoto(query, page, perPage).then(data => {
+//     const numberOfPage = Math.ceil(data.totalHits / perPage);
+//     refs.gallery.insertAdjacentHTML('beforeend', renderGalleryCards(data.hits));
+//     lightbox.refresh();
+
+//     if (page === numberOfPage) {
+//       refs.btnLoadMore.classList.add('is-hidden');
+//       Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+//       // btnLoadMore.removeEventListener('click', onClickLoadMore);
+//     };
+//   })
+//     .catch(onFetchError);
+// };
+
+
+
+
+
--- src/js/markup.js
+++ src/js/markup.js
@@ -1,38 +1,79 @@
-import { refs } from './refs';
-
-
 // функція створення розмітки
 
-
 export function renderGalleryCards(data) {
-    const markup = data.map(
+    return data.map(
       item =>
         `<div class="photo-card">
         <a href='${item.largeImageURL}'>
         <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" /></a>
         <div class="info">
           <p class="info-item">
-            <b>Likes<br>${item.likes}</b>
-          </p>
+            <b>Likes</b>${item.likes}
+            </p>
           <p class="info-item">
-            <b>Views<br>${item.views}</b>
-          </p>
+            <b>Views</b>${item.views}
+            </p>
           <p class="info-item">
-            <b>Comments<br>${item.comments}</b>
-          </p>
+            <b>Comments</b>
+            ${item.comments}
+            </p>
           <p class="info-item">
-            <b>Downloads<br>${item.downloads}</b>
-          </p>
+            <b>Downloads</b>
+            ${item.downloads}
+            </p>
         </div>
       </div>`
     )
     .join('');
-
-    refs.gallery.insertAdjacentHTML('beforeend', markup);
+ 
 };
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+// export function renderGalleryCards(data) {
+//     const markup = data.map(
+//       item =>
+//         `<div class="photo-card">
+//         <a href='${item.largeImageURL}'>
+//         <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" /></a>
+//         <div class="info">
+//           <p class="info-item">
+//             <b>Likes</b>${item.likes}
+//           </p>
+//           <p class="info-item">
+//                   <b>Views</b>
+//                   ${item.views}
+//                 </p>
+//                 <p class="info-item">
+//                   <b>Comments</b>
+//                   ${item.comments}
+//                 </p>
+//                 <p class="info-item">
+//                   <b>Downloads</b>
+//                   ${item.downloads}
+//                 </p>
+//         </div>
+//       </div>`
+//     )
+//     .join('');
+
+//     refs.gallery.insertAdjacentHTML('beforeend', markup);
+// };
+
+
+// const a = renderGalleryCards(data);
+// console.log(a);
 
 
 // export function createMarkup(searchResults) {
